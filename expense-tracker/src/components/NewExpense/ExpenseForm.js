1e210a8e18fc462c8e13d298cd20969e0f4815d6
adavis-pk <cubicle.jockey@live.com>
2022-04-26 14:39:15 -0700

    Submit

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -3,19 +3,19 @@ import './ExpenseForm.css'
 import { useState } from 'react';
 
 const ExpenseForm = () => {
-    // const [title, setTitle] = useState('');
-    // const [amount, setAmount] = useState('');
-    // const [date, setDate] = useState('');
+    const [inputTitle, setTitle] = useState('');
+    const [inputAmount, setAmount] = useState('');
+    const [inputDate, setDate] = useState('');
 
     //Use an inline object model
-    const[inputs, setInputs] = useState({
-         title: ''
-        ,amount: ''
-        ,date: ''
-    });
+    // const[inputs, setInputs] = useState({
+    //      title: ''
+    //     ,amount: ''
+    //     ,date: ''
+    // });
 
     const titleChangeHandler = (event) => {
-        //setTitle(event.target.value);
+        setTitle(event.target.value);
         // setInputs({
         //     ...inputs,
         //     title: event.target.value
@@ -23,26 +23,38 @@ const ExpenseForm = () => {
 
         //NOTE: React schedules changes, previous model could
         // cause the function to retrieve an outdated state.
-        setInputs((previousState) => {
-            return {...previousState, title: event.target.value };
-        });
+        // setInputs((previousState) => {
+        //     return {...previousState, title: event.target.value };
+        // });
     };
     const amountChangedHandler = (event) => {
-        //setAmount(event.target.value);
-        setInputs((previousState) => {
-            return { ...previousState, amount: event.target.value };
-        });
+        setAmount(event.target.value);
+        // setInputs((previousState) => {
+        //     return { ...previousState, amount: event.target.value };
+        // });
     };
 
     const dateChangedHandler = (event) => {
-        //setDate(event.target.value);
-        setInputs((previousState) => {
-            return { ...previousState, date: event.target.value };
-        });
+        setDate(event.target.value);
+        // setInputs((previousState) => {
+        //     return { ...previousState, date: event.target.value };
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault(); //prevent default page reload
+
+        const expense = {
+            title: inputTitle,
+            amount: inputAmount,
+            date: new Date(inputDate)
+        };
+
+        console.log(expense);
     };
 
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label id="title-label">Title:</label>
