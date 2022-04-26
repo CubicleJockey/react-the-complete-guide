af1470e7b774518813cc8c2641d54536c579d2a8
André Davis <CubicleJockey@users.noreply.github.com>
2022-04-25 22:44:04 -0700

    Checkpoint

---
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,43 @@
+import './ExpenseForm.css'
+
+import { useState } from 'react';
+
+const ExpenseForm = () => {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const titleChangeHandler = (event) => {
+
+    };
+    const amountChangedHandler = (event) => {
+
+    };
+
+    const dateChangedHandler = (event) => {
+
+    };
+
+    return(
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label id="title-label">Title:</label>
+                    <input type="text" aria-labelledby='title-label' onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label id="amount-label">Amount:</label>
+                    <input type="number" min="0.01" step="0.01" aria-labelledby='amount-label' onChange={amountChangedHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label id="date-label">Date:</label>
+                    <input type="date" min="2019-01-01" max="2022-12-31" aria-labelledby='date-label' onChange={dateChangedHandler}/>
+                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expense</button>
+            </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
