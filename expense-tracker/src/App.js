018004e78c89f63bb3cd2261dc53be0947b7bee9
adavis-pk <cubicle.jockey@live.com>
2022-04-26 18:47:27 -0700

    Assignment

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,19 +1,21 @@
+import { useState } from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import {v4 as Guid } from 'uuid';
 
 const App = () => {
-  const expenses = [
-     { id: Guid(), title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14) }
-    ,{ id: Guid(), title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) }
-    ,{ id: Guid(), title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)}
-    ,{ id: Guid(), title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 5, 12)}
-  ];
+  const [expenses, setExpenses] = useState([
+    { id: Guid(), title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14) }
+   ,{ id: Guid(), title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) }
+   ,{ id: Guid(), title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)}
+   ,{ id: Guid(), title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 5, 12)}
+ ])
 
   const addExpenseHandler = (expense) =>{
-    console.log('App.js');
-    console.log(expense);
-    //expenses.push(expense);
+     expenses.push(expense);
+     setExpenses(expenses);
+
+     console.log(expenses);
   };
 
   return (
--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import {v4 as Guid } from 'uuid';
+import Card from '../UI/Card';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState(2020);
+    
+    const filterChangedHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        console.log('Expenses.js');
+        console.log(`Selected Year: [${selectedYear}]`);
+    };
     return (
-        <div className="expenses">
-            {
-            props.items.map(e => {
-                return <ExpenseItem key={Guid()} title={e.title} amount={e.amount} date={e.date}/>
-            })
-            }
-        </div>
+        <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
+            { props.items.map(e => <ExpenseItem key={Guid()} title={e.title} amount={e.amount} date={e.date}/>) }
+        </Card>
     );
 }
 
