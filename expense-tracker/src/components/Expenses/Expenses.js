ce6ca75cd8a7001b734bfd18f6a76f0018223c91
adavis-pk <cubicle.jockey@live.com>
2022-04-27 11:24:53 -0700

    Refactoring

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -23,6 +23,9 @@ const Expenses = (props) => {
         else { return <NoExpenseItems />; }
     };
 
+    // {ternary condition allowed in curly braces}
+    // NOTE: JavaScript trick. Right side only happens if left is true 
+    //       filtered.length === 0 && <p>Some Message</p>
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
