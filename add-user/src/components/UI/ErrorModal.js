a0f2dd606dd4c99e43dc43c2a369f0f37ef17eed
adavis-pk <cubicle.jockey@live.com>
2022-05-03 16:00:29 -0700

    React Portals.

--- add-user/src/components/UI/ErrorModal.js
+++ add-user/src/components/UI/ErrorModal.js
@@ -2,25 +2,38 @@ import styles from './ErrorModal.module.css';
 
 import Card from "./Card";
 import Button from "./Button";
+
+import ReactDom from 'react-dom';
+
+const Backdrop = props => {
+    return (<div key="modal-backdrop" className={styles.backdrop}></div>);
+};
+
+const ModalOverlay = props => {
+    return (
+        <Card key="modal-card-content" className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <section className={styles.content}>
+                <p>{props.message}</p>
+            </section>
+            <footer className={styles.actions}>
+                <Button onClick={props.onConfirmClick}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
+
+//Portaling were this item will be rendered.
 const ErrorModal = (props) => {
-    return ( //NOTE: Fragment is using an array instead of a wrapper. Requires key property. Help prevent Div Soup
-        // <section id="error-modal">
-        [
-            <div key="modal-backdrop" className={styles.backdrop}></div>
-            
-            ,<Card key="modal-card-content" className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <section className={styles.content}>
-                    <p>{props.message}</p>
-                </section>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onConfirmClick}>Okay</Button>
-                </footer>
-            </Card>
-        // </section>
-        ]
+    return (
+        <>
+            { ReactDom.createPortal(<Backdrop />, document.getElementById('backdrop-root')) }
+            { ReactDom.createPortal(
+                    <ModalOverlay title={props.title} message={props.message} onConfirmClick={props.onConfirmClick} />
+                    ,document.getElementById('overlay-root')) }
+        </>
     );
 
 };
