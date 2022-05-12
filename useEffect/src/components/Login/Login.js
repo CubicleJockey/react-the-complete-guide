eae1b3031d0ffdbb7b96936e9e57b04bca57085c
adavis-pk <cubicle.jockey@live.com>
2022-05-12 10:57:39 -0700

    Prep for useReducer

--- useEffect/src/components/Login/Login.js
+++ useEffect/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
